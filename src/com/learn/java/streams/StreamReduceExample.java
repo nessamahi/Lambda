f736ae1f0a23d1593a3ab39d5fd404b59b564ca7
Nessa <nessamahi1@gmail.com>
2023-02-22 07:10:15 -0500

    Examples

--- src/com/learn/java/streams/StreamReduceExample.java
+++ src/com/learn/java/streams/StreamReduceExample.java
@@ -1,5 +1,8 @@
 package com.learn.java.streams;
 
+import com.learn.java.data.Student;
+import com.learn.java.data.StudentDatabase;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -22,12 +25,30 @@ public class StreamReduceExample {
                 .reduce( (a,b) -> a*b); //1 is default value, then is BiFunction, then we perform multiplication
     }
 
+    public static Optional<Student> getHighestGPA(){
+        return StudentDatabase.getAllStudents().stream()
+                .reduce((s1,s2) -> s1.getGpa() > s2.getGpa() ? s1 : s2);
+//                .reduce((s1,s2) -> {
+//                    if(s1.getGpa() > s2.getGpa()){
+//                        return s1;
+//                    }
+//                    else {
+//                        return s2;
+//                    }
+//                });
+    }
+
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1,3,5,7);
         System.out.println(performMultiplication(integers));
         Optional<Integer> res = performMultiplicationWithoutIdentity(integers);
         System.out.println(res.isPresent());
         System.out.println(res.get());
+
+        Optional<Student> studentOptional = getHighestGPA();
+        if(studentOptional.isPresent()){
+            System.out.println(studentOptional.get());
+        }
     }
 
 }
